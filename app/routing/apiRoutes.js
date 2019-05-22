779450a0a83c8a0b487c2314df930a6b69daed58
mdh206 <49321381+mdharris206@users.noreply.github.com>
2019-05-21 22:08:43 -0400

    working on match function

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,6 +14,36 @@ module.exports = function(app) {
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body parsing middleware
     console.log(req.body)
+    /*
+      Take current user scores array
+      Loop through the other existing users scores array
+      Calculate the difference each time
+      Store that difference value 
+      Compare difference value for lowest and replace if lower
+      return the lowest value for best match
+      send that back to display on page      
+    */
+    let scores = req.body.scores;
+    let minDiff = 40;
+    let match;
+    for(let i =0; i < friendsData.length; i++ ){
+        console.log(friendsData[i].scores);
+        let arraySum = 0; 
+      for(let j = 0; j < friendsData[i].scores.length; j++ ){
+
+        arraySum += friendsData[i].scores[j];
+        
+        console.log(
+          friendsData[i].scores[j]);
+      };
+      if(minDiff > arraySum){
+        minDiff = arraySum;
+        match = friendsData[i];
+      };
+    };
+
+    res.send(match.name + ''+match.photo);
+
   });
 
   // ---------------------------------------------------------------------------
